362e2197dadcbde0e098b985996a2ed72dfe0899
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-16 13:00:08 +0100

    initinal

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCz9H9V7JMYySgoJ2mbcbU1-7io4WUr1So",
+    authDomain: "feu16-3e88e.firebaseapp.com",
+    databaseURL: "https://feu16-3e88e.firebaseio.com",
+    storageBucket: "feu16-3e88e.appspot.com",
+    messagingSenderId: "78285945611"
+  };
+  firebase.initializeApp(config);
+</script>
