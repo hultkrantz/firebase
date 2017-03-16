3e5c5d026f0d1b90c13f149f76f51f2a5fcc2cfc
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-16 13:30:13 +0100

    adding

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 
-<script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"></script>
-<script>
+script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCz9H9V7JMYySgoJ2mbcbU1-7io4WUr1So",
@@ -10,4 +10,15 @@
     messagingSenderId: "78285945611"
   };
   firebase.initializeApp(config);
-</script>
+
+
+
+// Ett objekt för att hantera GitHub-autentisering
+let provider = new firebase.auth.GithubAuthProvider();
+// Skapa ett Promise som visar ett popup-fönster
+// Obs! Kontrollera att fönstret inte blockeras av en ad blocker
+firebase.auth().signInWithPopup(provider)
+.then(function(result) {
+    // Om autentisering lyckas, så finns användarinfo i user
+    let user = result.user;
+});
