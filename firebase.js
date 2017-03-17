6e92fc2d12278ec629a19f1162f7034c66c6c291
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-17 10:11:40 +0100

    a

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-
 var config = {
 	apiKey: "AIzaSyCz9H9V7JMYySgoJ2mbcbU1-7io4WUr1So"
 	, authDomain: "feu16-3e88e.firebaseapp.com"
@@ -6,43 +5,33 @@ var config = {
 	, storageBucket: "feu16-3e88e.appspot.com"
 	, messagingSenderId: "78285945611"
 };
-console.log("second");
 firebase.initializeApp(config);
-// Ett objekt för att hantera GitHub-autentisering
-let provider = new firebase.auth.GithubAuthProvider();
-// Skapa ett Promise som visar ett popup-fönster
-// Obs! Kontrollera att fönstret inte blockeras av en ad blocker
-firebase.auth().signInWithPopup(provider).then(function (result) {
-	// Om autentisering lyckas, så finns användarinfo i user
-	Console.log("Lyckades")
-	let user = result.user;
-}).catch(function (error) {
-	console.log(error);
-});
-
-///
-///
-///
-// Initialize Firebase
-var namn = document.getElementById("inputNamn")
-	, familj = document.getElementById("inputFamilj")
-	, antal = document.getElementById("inputAntal")
-	, farg = document.getElementById("inputFarg")
-	, addButton = document.getElementById("addButton");
-console.log("first");
-//
-addButton.addEventListener("click", function () {
-	console.log("Klickat läg till djur")
-	firebase.database().ref('djur/' + namn.value).set({
-		namn: namn.value
-		, familj: familj.value
-		, antal: antal.valu
-		, färg: farg.value
+window.addEventListener('load', function () {
+	let namn = document.getElementById('inputNamn');
+	let familj = document.getElementById('inputFamilj');
+	let antal = document.getElementById('inputAntal');
+	let färg = document.getElementById('inputFärg');
+	let addButton = document.getElementById('addButton');
+	let tableVisaDjur = document.getElementById('tableVisaDjur');
+	addButton.addEventListener('click', function (event) {
+		console.log('Klickat lägga till djur');
+		firebase.database().ref('djur/' + namn.value).set({
+			namn: namn.value
+			, familj: familj.value
+			, antal: antal.value
+			, färg: färg.value
+		})
+	});
+	firebase.database().ref('djur/').on('value', function (snapshot) {
+		console.log('Första gången eller ändring i databasen');
+		let data = snapshot.val();
+		tableVisaDjur.innerHTML = '';
+		for (let key in data) {
+			let animal = data[key];
+			console.log('data[key]=', data[key]);
+			let tr = document.createElement('tr');
+			tr.innerHTML = `<td>${animal.namn}</td> <td>${animal.familj}</td> <td>${animal.antal}</td> <td style="width: 50px; background-color: ${animal.färg};"></td>`;
+			tableVisaDjur.appendChild(tr);
+		}
 	})
-	firebase.database().ref("djur").on("value", function (snapchot) {})
-	console.log("Ändring i databasen")
-	let data = snapchot.val(); // göt om till ett objekt.
-	for ( let key in data ) {
-		let animal = data[key];
-	}
 });
