d2de178d5265828f07fe91bd5332151ed05315ac
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-17 13:46:11 +0100

    ja

--- firebase.js
+++ firebase.js
@@ -1,11 +1,4 @@
-var config = {
-	apiKey: "AIzaSyCz9H9V7JMYySgoJ2mbcbU1-7io4WUr1So"
-	, authDomain: "feu16-3e88e.firebaseapp.com"
-	, databaseURL: "https://feu16-3e88e.firebaseio.com"
-	, storageBucket: "feu16-3e88e.appspot.com"
-	, messagingSenderId: "78285945611"
-};
-firebase.initializeApp(config);
+
 window.addEventListener('load', function () {
 	let namn = document.getElementById('inputNamn');
 	let familj = document.getElementById('inputFamilj');
